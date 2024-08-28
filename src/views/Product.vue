edd2f85a8a08b826a7870db4a32ddb7a9549688e
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-28 11:41:50 +0200

    Update Product.vue

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,10 +2,18 @@
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+const productId = route.params.id;
+const productData = ref(null) // Product Data goes here.
+
+/*
+Fetch data yada yada!
+*/
+
+const route = useRoute();
 </script>
 
 <template>
 	<div>
-		<h1>Product Details for</h1>
+		<h1>Product Details for {{ productId }}</h1>
 	</div>
 </template>
