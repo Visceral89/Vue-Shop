89cdf3b757944254f21671d91a8a8b53b6af4347
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-28 12:07:19 +0200

    boilerplate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <script setup></script>
 
 <template>
-	<div>Home</div>
+	<section id="most-popular">
+		<h2>Most Popular</h2>
+	</section>
+	<section id="highlights">New</section>
+	<section id="testimonials">
+		<div>Best products</div>
+		<div>Safe Deliveries</div>
+		<div>No animals harmed</div>
+	</section>
+	<section id="articles">
+		<h2>Recent Articles</h2>
+		<div class="article-container">
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+		</div>
+		<button>all articles</button>
+	</section>
 </template>
 
 <style></style>
