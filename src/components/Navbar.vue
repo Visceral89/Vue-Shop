f03a6d83ebe24167112caf52440ef61012b352cc
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-28 11:50:54 +0200

    Navbar Css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,16 +2,40 @@
 
 <template>
 	<nav>
-		<div>Search</div>
-		<div>Men Women Children</div>
-		<div>Logo</div>
-		<div>Categories</div>
+		<div class="toprow">
+			<div>Search</div>
+			<div>Men Women Children</div>
+			<div>Logo</div>
+		</div>
+		<div class="botrow">
+			<div>Categories</div>
+		</div>
 	</nav>
 </template>
 
 <style scoped>
 nav {
 	width: 100%;
+	height: 100px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 
+	padding: 0 10rem;
+}
+.toprow {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+}
+.botrow {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
